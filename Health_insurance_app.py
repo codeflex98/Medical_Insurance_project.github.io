4cc0079be9a0bc7b5f841a4f3b486a2e1d1fcfbb
Code.manu <164360071+codeflex98@users.noreply.github.com>
2024-11-15 21:19:31 +0100

    Update Health_insurance_app.py

--- Health_insurance_app.py
+++ Health_insurance_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import joblib
 import numpy as np
-import pandas as pd
 import os
 
-# Load the trained model
 model_path = os.path.join(os.path.dirname(__file__), 'health_insurance_model')
 model = joblib.load(model_path)
 
@@ -20,33 +18,24 @@ def main():
     s2 = st.selectbox("Are you a smoker?", ("Yes", "No"))
     p5 = 1 if s2 == "Yes" else 0
     s6 = st.selectbox("Enter your region", ("Southwest", "Southeast", "Northwest", "Northeast"))
-
     if s6 == "Southwest":
         p6 = 0
     elif s6 == "Southeast":
         p6 = 1
     elif s6 == "Northwest":
         p6 = 2
-    else:  # "Northeast"
+    else:
         p6 = 3
 
-     # Ensure input values are correctly typed
-    p1 = int(p1)
-    p3 = float(p3)
-    p4 = int(p4)
-    p5 = int(p5)
-    p6 = int(p6)
-
-    # Input array converted to DataFrame
+    # Input array
     input_data = np.array([[p1, p2, p3, p4, p5, p6]])
-    columns = ["age", "sex", "bmi", "children", "smoker", "region"]
-    input_df = pd.DataFrame(input_data, columns=columns)
 
     # Predict button
     if st.button("Predict"):
         try:
-            prediction = model.predict(input_df)
-            st.success(f"The predicted insurance cost is: ${prediction[0]:,.2f}")
+            prediction = model.predict(input_data)
+            st.balloons() 
+            st.success(f"Your insurance cost is {round(prediction[0], 2)} US Dollars")
         except Exception as e:
             st.error(f"Error in prediction: {e}")
 
